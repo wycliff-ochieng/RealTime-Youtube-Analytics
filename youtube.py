c26f66d46e0dc17d20ecb2311707c4c7d58d54ca
wycliff <wyckieochieng91@gmail.com>
2025-04-26 08:05:59 +0300

    fetching data

--- youtube.py
+++ youtube.py
@@ -1,5 +1,7 @@
 import  requests
 import yaml
+import json
+from pprint import pprint
 
 with open('config.yaml', 'r') as f:
     config = yaml.safe_load(f)
@@ -9,8 +11,20 @@ if __name__=="__main__":
     videos = requests.get('https://www.googleapis.com/youtube/v3/videos',
                       {
                           'key':api_key,
-                          'id':'-368VzqcjSo&list=PL0xRBLFXXsP5PwGwZkSOhPFbvoUUKQJat',
+                          'id':'r39z7CX1tZo',
                           'part':'snippet,statistics,status'
                       })
-    print(videos.text)
+    #print(videos.text)
+    response = json.loads(videos.text)['items']
+    for vids in response:
+        vids_res = {
+            'title':vids['snippet']['title'],
+            'timePublished':vids['snippet']['publishedAt'],
+            'likes':vids['statistics'].get('likeCount',0),
+            'comments':vids['statistics'].get('commentCount',0),
+            'views':vids['statistics'].get('viewCount',0),
+            'tags':vids['snippet']['tags'][0:2]
+            }
+        
+    print(pprint(vids_res))
 
